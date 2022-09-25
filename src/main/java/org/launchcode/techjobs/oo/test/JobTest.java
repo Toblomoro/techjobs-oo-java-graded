9ef6105d752d30b0bd195de8c359a6c2b83d5662
Ben Voges <108232180+Toblomoro@users.noreply.github.com>
2022-09-25 13:10:02 -0500

    Started jobtest assertnotequals looks good

--- src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
+++ src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
@@ -1,12 +1,23 @@
 package org.launchcode.techjobs.oo.test;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
+import org.launchcode.techjobs.oo.Job;
+
+import java.util.Objects;
+
+import static org.junit.Assert.*;
 
 /**
  * Created by LaunchCode
  */
 @RunWith(JUnit4.class)
 public class JobTest {
+    Job job1, job2, Job3, job4;
 
+    @Test
+    public void testSettingJobId() {
+        assertNotEquals(job1, job2);
+    }
 }
